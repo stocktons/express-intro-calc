3527f94536daf37e3d886be79e9592d55e2e026d
stocktons <sarah@thanksforallthe.fish>
2021-07-28 15:36:13 -0400

    mean function completed

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const app = express();
 
 // useful error class to throw
 const { NotFoundError } = require("./expressError");
+const { findMean,findMedian,findMode} = require("./stats")
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
+app.get("/mean", function (req, res) {
+  console.log(req.query);
+  let numsArr = req.query.nums.split(",");
+  let nums = numsArr.map(n => +n);
+  return res.send({ operation : "mean", value : findMean(nums) })
+})
 
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
@@ -19,8 +26,8 @@ const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
-app.use(function (req, res, next) {
-  return next(new NotFoundError());
+app.use(function (req, res, next) { // do we need any of these params? 
+  throw new NotFoundError();
 });
 
 /** Error handler: logs stacktrace and returns JSON error message. */
